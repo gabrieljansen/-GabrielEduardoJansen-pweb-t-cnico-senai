ac24acd2f849cea4c115326f0476c759cfd382f0
Gabriel <you@example.com>
2019-11-28 16:47:16 -0200

    codigo

---
+++ apps-slim-base-master/src/routes.php
@@ -0,0 +1,40 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+
+  $container = $app->getContainer();
+
+  $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+    // Sample log message
+    $container->get('logger')->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $container->get('renderer')->render($response, 'index.phtml', $args);
+  });
+
+  $app->post('/inserir/', function (Request $request, Response $response, array $args) use ($container) {
+    // Sample log message
+    $container->get('logger')->info("Slim-Skeleton '/' route");
+
+    $params = $request->getParsedBody();
+    $modelo = $params['modelo'];
+    $marca = $params['marca'];
+    $ano = $params['ano'];
+    $conexao = $container->get('pdo');
+
+        $result = $conexao->query('INSERT INTO carro  (modelo, marca, ano) 
+                                   VALUES ( "'. $params['modelo'] .'",   "'.  $params['marca'] .'",   "'.  $params['ano'] .'")');
+       
+       
+
+       
+       return $response->withRedirect('/listar/');
+    
+
+  });
+
+};
---
+++ apps-slim-base-master/src/routesListar.php
@@ -0,0 +1,31 @@
+<?php
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    $app->get('/listar/', function (Request $request, Response $response, array $args) use ($container) {
+
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+
+
+        $conexao = $container->get('pdo');
+
+      
+
+        $listar = $conexao->query('SELECT * FROM carro where id ')->fetchAll();
+
+        $args['carro'] = $listar;
+
+
+        // Render index view
+        return $container->get('renderer')->render($response, 'index2.phtml', $args);
+
+
+    });
+
+};
